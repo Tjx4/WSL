8b86467236b82874e7e41c3f23ff160c596d8cb4
Tshepo <rocboyt@gmail.com>
2020-08-29 13:48:25 +0200

    Added location recyclerView

--- app/src/main/java/co/za/rain/myapplication/adapters/LocationsAdapter.kt
+++ app/src/main/java/co/za/rain/myapplication/adapters/LocationsAdapter.kt
@@ -1,4 +1,58 @@
 package co.za.rain.myapplication.adapters
 
-class LocationsAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import co.za.rain.myapplication.R
+import co.za.rain.myapplication.models.UserLocation
+
+class LocationsAdapter(context: Context, private val userLocations: List<UserLocation>) : RecyclerView.Adapter<LocationsAdapter.ViewHolder>() {
+
+    private val layoutInflater: LayoutInflater = LayoutInflater.from(context)
+    private var locationClickListener: LocationClickListener? = null
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = layoutInflater.inflate(R.layout.location_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val userLocation = userLocations[position]
+        holder.locationNameTv.text = userLocation.name
+        holder.locationDescriptionTv.text = userLocation.description
+        holder.locationDateTimeTv.text = userLocation.dateTime
+    }
+
+    inner class ViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        internal var locationNameTv = itemView.findViewById<TextView>(R.id.tvLocationName)
+        internal var locationDescriptionTv = itemView.findViewById<TextView>(R.id.tvLocationDescription)
+        internal var locationDateTimeTv = itemView.findViewById<TextView>(R.id.tvLocationDateTime)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(view: View) {
+            locationClickListener?.onServiceCategoryClick(view, adapterPosition)
+        }
+    }
+
+    internal fun getItem(id: Int): UserLocation? {
+        return userLocations[id]
+    }
+
+    interface LocationClickListener {
+        fun onServiceCategoryClick(view: View, position: Int)
+    }
+
+    fun setLocationClickListener(locationClickListener: LocationClickListener) {
+        this.locationClickListener = locationClickListener
+    }
+
+    override fun getItemCount() = userLocations.size
+
 }
--- app/src/main/java/co/za/rain/myapplication/features/locationTracker/LocationTrackerActivity.kt
+++ app/src/main/java/co/za/rain/myapplication/features/locationTracker/LocationTrackerActivity.kt
@@ -4,15 +4,21 @@ import android.location.Location
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.SnapHelper
 import co.za.rain.myapplication.R
+import co.za.rain.myapplication.adapters.LocationsAdapter
 import co.za.rain.myapplication.extensions.DEFAULT_STATUS_BAR_ALPHA
 import co.za.rain.myapplication.extensions.setTranslucentStatusBar
 import co.za.rain.myapplication.features.base.activity.BaseMapActivity
+import co.za.rain.myapplication.models.UserLocation
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import kotlinx.android.synthetic.main.activity_location_tracker.*
 
-class LocationTrackerActivity : BaseMapActivity() {
+class LocationTrackerActivity : BaseMapActivity(), LocationsAdapter.LocationClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_location_tracker)
@@ -25,6 +31,24 @@ class LocationTrackerActivity : BaseMapActivity() {
         else{
             Toast.makeText(this, "Google play services unavailable", Toast.LENGTH_LONG).show()
         }
+    }
+
+    fun onLocationsUpdated(locations: List<UserLocation>) {
+        var  locationsAdapter = LocationsAdapter(this, locations)
+        locationsAdapter.setLocationClickListener(this)
+
+        val categoryLayoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        categoryLayoutManager.initialPrefetchItemCount = locations.size
+
+        rvLocations?.layoutManager = categoryLayoutManager
+        rvLocations?.adapter = locationsAdapter
+
+        val snapHelper: SnapHelper = PagerSnapHelper()
+        snapHelper.attachToRecyclerView(rvLocations)
+    }
+
+    override fun onServiceCategoryClick(view: View, position: Int) {
+        TODO("Not yet implemented")
     }
 
     fun onSaveLocationButtonClicked(view: View) {
@@ -83,4 +107,5 @@ class LocationTrackerActivity : BaseMapActivity() {
       //      moveUserMarker(currentCoordinates)
       //  }
     }
+
 }
