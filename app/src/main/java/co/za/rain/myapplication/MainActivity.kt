5387630e02cb548f8395b9d304131f4097c5e075
Tshepo <rocboyt@gmail.com>
2020-08-30 07:09:26 +0200

    Added signal check layout

--- app/src/main/java/co/za/rain/myapplication/MainActivity.kt
+++ app/src/main/java/co/za/rain/myapplication/MainActivity.kt
@@ -5,12 +5,13 @@ import android.os.Bundle
 import co.za.rain.myapplication.extensions.FADE_IN_ACTIVITY
 import co.za.rain.myapplication.extensions.navigateToActivity
 import co.za.rain.myapplication.features.locationTracker.LocationTrackerActivity
+import co.za.rain.myapplication.features.signalStrength.SignalStrengthActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigateToActivity(LocationTrackerActivity::class.java, FADE_IN_ACTIVITY)
+        navigateToActivity(SignalStrengthActivity::class.java, FADE_IN_ACTIVITY)
     }
 }
