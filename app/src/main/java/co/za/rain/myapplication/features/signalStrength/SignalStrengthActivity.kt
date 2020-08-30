52c581ea0314d49e278ddcbd67c219f823d30b08
Tshepo <rocboyt@gmail.com>
2020-08-30 11:32:32 +0200

    Pre logic fix

--- app/src/main/java/co/za/rain/myapplication/features/signalStrength/SignalStrengthActivity.kt
+++ app/src/main/java/co/za/rain/myapplication/features/signalStrength/SignalStrengthActivity.kt
@@ -1,15 +1,25 @@
 package co.za.rain.myapplication.features.signalStrength
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
-import android.telephony.CellSignalStrength
-import android.telephony.CellSignalStrengthLte
-import android.telephony.TelephonyManager
+import android.os.Parcel
+import android.os.Parcelable
+import android.telephony.*
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import co.za.rain.myapplication.R
 import co.za.rain.myapplication.databinding.ActivitySignalStrengthBinding
 import co.za.rain.myapplication.features.base.activity.BaseChildActivity
+import co.za.rain.myapplication.helpers.Connectivity
 import kotlinx.android.synthetic.main.activity_signal_strength.*
+import java.lang.reflect.Method
 
 
 class SignalStrengthActivity : BaseChildActivity() {
@@ -33,6 +43,79 @@ class SignalStrengthActivity : BaseChildActivity() {
 
         supportActionBar?.title = "Signal monitor"
 
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+            1
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        for (i in permissions.indices) {
+            val permission = permissions[i]
+            val grantResult = grantResults[i]
+
+            if (permission == Manifest.permission.ACCESS_COARSE_LOCATION) {
+                if (grantResult == PackageManager.PERMISSION_GRANTED) {
+                    checkPhoneSignal()
+                } else {
+                    onLocationPermissionDenied()
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun checkPhoneSignal(){
+
+        var connectionType = ""
+        if(Connectivity.isConnectedWifi(this)){
+            connectionType = "WIFI"
+        }
+        else if(Connectivity.isConnectedMobile(this)){
+            connectionType = "Mobile"
+        }
+
+        var info = Connectivity.getNetworkInfo(this)
+        //info.subtype.
+        var dfdf = Connectivity.isConnected(this)
+        var dd = "$connectionType $info $dfdf"
+
+        val tm = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
+        val cellInfoList = tm.allCellInfo
+
+        var ddd = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+            tm.signalStrength?.cellSignalStrengths
+        } else {
+
+        }
+
+        for (cellInfo in cellInfoList) {
+            if (cellInfo is CellInfoLte) {
+                var cellSignalStrengthLte = cellInfo.cellSignalStrength as CellSignalStrengthLte
+
+                tvMobileDataRSRPSpeed.text = "${cellSignalStrengthLte.rsrp} mb"
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+                    tvMobileDataRSSISpeed.text = "${cellSignalStrengthLte.rssi} mb"
+                }
+                else{
+                    //Todo: get RSSI for < Q
+                    tvMobileDataRSSISpeed.text = "0 mb"
+                }
+            }
+            else if (cellInfo is CellInfoGsm) {
+                var cellSignalStrength = cellInfo.cellSignalStrength
+                tvMobileDataRSRPSpeed.text = "${cellSignalStrength.dbm} mb"
+                tvMobileDataRSSISpeed.text = "${cellSignalStrength.asuLevel} mb"
+            }
+        }
+/*
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P && android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
 
             val cellInfoList: List<CellSignalStrength>
@@ -47,6 +130,10 @@ class SignalStrengthActivity : BaseChildActivity() {
                 }
             }
         }
+*/
+    }
 
+    fun onLocationPermissionDenied(){
+        Toast.makeText(this, "onLocationPermissionDenied", Toast.LENGTH_LONG).show()
     }
 }
---
+++ app/src/main/java/co/za/rain/myapplication/helpers/Connectivity.kt
@@ -0,0 +1,78 @@
+package co.za.rain.myapplication.helpers
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import android.telephony.*
+import androidx.appcompat.app.AppCompatActivity
+
+
+object Connectivity {
+
+    fun getNetworkInfo(context: Context): NetworkInfo {
+        val cm = context.getSystemService(AppCompatActivity.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return cm.activeNetworkInfo
+    }
+
+    fun isConnected(context: Context): Boolean {
+        val info = getNetworkInfo(context)
+        return info != null && info.isConnected
+    }
+
+    fun isConnectedWifi(context: Context): Boolean {
+        val info = getNetworkInfo(context)
+        return info != null && info.isConnected && info.type == ConnectivityManager.TYPE_WIFI
+    }
+
+    fun isConnectedMobile(context: Context): Boolean {
+        val info = getNetworkInfo(context)
+        return info != null && info.isConnected && info.type == ConnectivityManager.TYPE_MOBILE
+    }
+
+    fun isConnectedFast(context: Context): Boolean {
+        val info = getNetworkInfo(context)
+        return info != null && info.isConnected && isConnectionFast(info.type, info.subtype)
+    }
+
+    fun isConnectionFast(type: Int, subType: Int): Boolean {
+        return if (type == ConnectivityManager.TYPE_WIFI) {
+            true
+        } else if (type == ConnectivityManager.TYPE_MOBILE) {
+            when (subType) {
+                TelephonyManager.NETWORK_TYPE_1xRTT -> false // ~ 50-100 kbps
+                TelephonyManager.NETWORK_TYPE_CDMA -> false // ~ 14-64 kbps
+                TelephonyManager.NETWORK_TYPE_EDGE -> false // ~ 50-100 kbps
+                TelephonyManager.NETWORK_TYPE_EVDO_0 -> true // ~ 400-1000 kbps
+                TelephonyManager.NETWORK_TYPE_EVDO_A -> true // ~ 600-1400 kbps
+                TelephonyManager.NETWORK_TYPE_GPRS -> false // ~ 100 kbps
+                TelephonyManager.NETWORK_TYPE_HSDPA -> true // ~ 2-14 Mbps
+                TelephonyManager.NETWORK_TYPE_HSPA -> true // ~ 700-1700 kbps
+                TelephonyManager.NETWORK_TYPE_HSUPA -> true // ~ 1-23 Mbps
+                TelephonyManager.NETWORK_TYPE_UMTS -> true // ~ 400-7000 kbps
+                TelephonyManager.NETWORK_TYPE_EHRPD -> true // ~ 1-2 Mbps
+                TelephonyManager.NETWORK_TYPE_EVDO_B -> true // ~ 5 Mbps
+                TelephonyManager.NETWORK_TYPE_HSPAP -> true // ~ 10-20 Mbps
+                TelephonyManager.NETWORK_TYPE_IDEN -> false // ~25 kbps
+                TelephonyManager.NETWORK_TYPE_LTE -> true // ~ 10+ Mbps
+                TelephonyManager.NETWORK_TYPE_UNKNOWN -> false
+                else -> false
+            }
+        } else {
+            false
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun getGSMLevel(telephonyManager : TelephonyManager): Int {
+        return when (val info = telephonyManager?.allCellInfo?.firstOrNull()) {
+            is CellInfoLte -> info.cellSignalStrength.level
+            is CellInfoGsm -> info.cellSignalStrength.level
+            is CellInfoCdma -> info.cellSignalStrength.level
+            is CellInfoWcdma -> info.cellSignalStrength.level
+            else -> 0
+        }
+    }
+}
+
+
